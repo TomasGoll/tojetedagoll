1f4ac087ee11b1161fad1a05ddb2f862d2a6fbc0
TomasGoll <gotomasgoll@gmail.com>
2016-07-28 20:08:28 +0200

    Added WeddingGift Model, Created GiftRepository, updated PageController

---
+++ TojeTedaGoll/Data/TojeTedaGollRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TojeTedaGoll.Models;
+
+namespace TojeTedaGoll.Data
+{
+    public class TojeTedaGollRepository
+    {
+        private static WeddingGift[] _weddingGifts = new WeddingGift[]
+        {
+            new WeddingGift()
+            {
+                Id = 1,
+                GiftName = "Nadobi",
+                ShortDescription = "Nadobi od firmy XY",
+                FullDescription = "Lorem Ipsum dolor sit Amer",
+            },
+
+            new WeddingGift()
+            {
+                Id = 2,
+                GiftName = "Matrace",
+                ShortDescription = "Matrace z IKEI",
+                FullDescription = "Lorem Ipsum dolor sit Amer",
+            },
+
+            new WeddingGift()
+            {
+                Id = 3,
+                GiftName = "Cash",
+                ShortDescription = "Peníze a zase jen peníze, hodně peněz",
+                FullDescription = "Lorem Ipsum dolor sit Amer",
+            }
+        };
+
+        public WeddingGift[] GetWeddingGifts()
+        {
+            return _weddingGifts;
+        }
+    }
+}
---
+++ TojeTedaGoll/Models/WeddingGift.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TojeTedaGoll.Models
+{
+    public class WeddingGift
+    {
+        public int Id { get; set; }
+        public string GiftName { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public bool Chosen { get; set; }
+
+        public string GetGiftName
+        {
+            get
+            {
+                return GiftName;
+            }
+        }
+
+        public string GiftImageName
+        {
+            get
+            {
+                return GiftName.Replace(" ", "-").ToLower() + ".png";
+            }
+        }
+    }
+}
