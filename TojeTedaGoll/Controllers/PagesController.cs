fbaf7e280bf0b96dacf8bf66153ef9ba733ca0f5
TomasGoll <gotomasgoll@gmail.com>
2016-08-01 09:53:28 +0200

    adding whole folder

---
+++ TojeTedaGoll/Controllers/PagesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TojeTedaGoll.Data;
+
+namespace TojeTedaGoll.Controllers
+{
+    public class PagesController : Controller
+    {
+        private TojeTedaGollRepository _tojeTedaGollRepository = null;
+
+        public PagesController()
+        {
+            _tojeTedaGollRepository = new TojeTedaGollRepository();
+        }
+               
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Gifts()
+        {
+            var weddinGifts = _tojeTedaGollRepository.GetWeddingGifts();   
+            return View(weddinGifts);
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+    }
+}
